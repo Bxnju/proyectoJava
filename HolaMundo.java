a9514bc9aaa69385591cb9d7c80a905eefd4c79a
Bxnju <andres.benjumea@upb.edu.co>
2022-03-22 14:26:49 -0500

    Condicional IF

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 
 public class HolaMundo{
     public static void main(String[] args){
+
        Scanner in = new Scanner(System.in);
        System.out.println("Ingrese su nombre: ");
        String name = in.nextLine();
-       System.out.println("Bienvenido a nuestro sistema " + name + ". Por favor escoja una de las opciones a continuacion:");
+       System.out.println("Bienvenido a nuestro sistema " + name);
 
-       System.out.println("Si desea saber cual es su signo presione 1");
-        Int option = in.nextLine();
+       System.out.println("Si desea conocer su signo zodiacal marque 1");
+        String number = in.nextLine();
+        int option = Integer.parseInt(number);
 
         if(option == 1){
-            System.out.println("Ha entrado al sistema que reconoce su signo zodiacal.");
-            System.out.println("Por favor digite el mes en el que nacio");
+            System.out.print("Listo.");
         }
-
+     
 
     }
 }
